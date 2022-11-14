adac5d83da17dea293618c2bdd7e5841ba336bde
tarzanturk <70955017+FurkanKalay@users.noreply.github.com>
2022-11-14 22:27:38 +0100

    header en footer af

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <link rel="stylesheet"  href="css/footer.css">
+</head>
+    
+
+<footer> 
+    
+    <div class="footerleft">
+        <p class="title"> Go write something usefull</p>
+        <p>GalaxyData is the internet's info library that is used by <br> 
+             millions of people </p>
+    </div>
+    <div class="footerright">
+        All rights reserved 2022
+    </div>
+   
+   
+</footer>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <link rel="stylesheet"  href="css/header.css">
+</head>
+
+    
+<header> 
+    <div class="logo">
+        <h1>Galaxy</h1><h1 class="secondletter">Data</h1>
+    </div>
+    
+<nav> 
+    
+    <ul class="nav__links">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+   
+</nav>
+<div class="buttons">
+    <a class="atc" href="#"><button>Log in</button></a>
+    <a class="cta" href="#"><button>Sign in</button></a>
+</div>
+    
+</header>
