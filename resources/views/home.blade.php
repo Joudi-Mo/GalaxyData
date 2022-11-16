a77d05d7af7a614290d5458f7b9ff4c1d79d3097
Furkan <furkaneminkalay@gmail.com>
2022-11-16 11:41:52 +0100

    bezig met searchbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="css/home.css">
+    <script src="https://kit.fontawesome.com/3f5b3fe9f7.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 
@@ -16,6 +17,7 @@
     <div class="home">
         <div class="search">
             1
+            <i class="fa-solid fa-magnifying-glass"></i>
 
         </div>
         <div class="artikels">
