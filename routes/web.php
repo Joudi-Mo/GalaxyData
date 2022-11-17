b95f5a94a4d1e37847e57dbc294254a73512c6f7
tarzanturk <70955017+FurkanKalay@users.noreply.github.com>
2022-11-17 15:08:22 +0100

    signup bug fixed en header bug fixed

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/', function () {
-    return view('layout');
+Route::get('/about', function () {
+    return view('contact');
 });
 
 Route::get('/about', function () {
