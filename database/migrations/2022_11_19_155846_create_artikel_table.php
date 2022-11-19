e348a4ca5d32bd604c024f8dc90fca984fda9539
tarzanturk <70955017+FurkanKalay@users.noreply.github.com>
2022-11-19 19:01:20 +0100

    bezig met database

---
+++ database/migrations/2022_11_19_155846_create_artikel_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->id();
+            $table->id('likes');
+            $table->id('dislikes');
+            $table->string('title');
+            $table->longText('content');
+            $table->boolean('populair_artikel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+};
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,71 +10,15 @@
     <!-- fontawesome.com -->
     <script src="https://kit.fontawesome.com/a333f4247d.js" crossorigin="anonymous"></script>
 </head>
-<?php
-// Initialize the session
-session_start();
-$_SESSION = [];
-SESSION_destroy();
-session_start();
-$_SESSION["is_logged_in"] = false;
-$_SESSION["id"] = null;
-$_SESSION["username"] = null;
-$_SESSION["role"] = null;
-$username = $password = $login_err = "";
-// Include database file
-
-
-if (isset($_POST['submit']) && !empty(trim($_POST["email"])) && !empty(trim($_POST["pass"]))) {
-    $email = trim($_POST["email"]);
-    $password = trim($_POST["pass"]);
-
-    $sql = "SELECT id, firstname, password, email, role FROM `users` where email = '$email'";
-    if ($result = mysqli_query($conn, $sql)) {
-        $data = mysqli_fetch_assoc($result);
-    }
-
-
-    if (!is_null($data) || !empty($data)) {
-        if ($email == $data['email'] && $password == $data['password']) {
-            // Store data in session variables
-            $_SESSION["is_logged_in"] = true;
-            $_SESSION["id"] = $data['id'];
-            $_SESSION["username"] = $data['firstname'];
-            $_SESSION["role"] = $data['role'];
-
-            if ($data['role'] == 'klant') {
-                header("location: klant/bestellen.php");
-            } else {
-                header("location: medewerker/producten_overzicht.php");
-            }
-        } else {
-            $login_err = "Email of wachtwoord niet correct!";
-        }
-    }
-    else{
-        $login_err = "Deze email bestaat niet";
-    }
-
-    mysqli_close($conn); // Sluit de database verbinding
-} elseif (isset($_POST['submit'])) {
-    $login_err = "Een van de velden of beide is leeg";
-}
-?>
 
 <body id="body">
     <div class="wrapper">
         <h2>Sign Up</h2>
-        <form action="login.php" method="POST">
-            <p class="err-text">
-                <?php
-                if (!empty($login_err)) {
-                    echo $login_err;
-                }
-                ?>
-            </p>
+        <form action="/users" method="POST">
+           @csrf
             <div class="input-box">
                 <label for="username">Username</label>
-                <input name="text" type="text" placeholder="Enter your username">
+                <input name="username" type="text" placeholder="Enter your username">
             </div>
             <div class="input-box">
                 <label for="username">Email</label>
@@ -82,7 +26,7 @@ if (isset($_POST['submit']) && !empty(trim($_POST["email"])) && !empty(trim($_PO
             </div>
             <div class="input-box">
                 <label for="username">Password</label>
-                <input name="pass" type="password" placeholder="Enter your password">
+                <input name="password" type="password" placeholder="Enter your password">
             </div>
 
             <div class="input-box button">
